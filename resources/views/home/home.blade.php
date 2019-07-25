6fd6a3fade5a0366a8dc7557b7b1435cfe176655
Danilo Eduardo <daniloedu@gmail.com>
2019-07-25 19:56:29 -0300

    Acerto de layout e inicio do desenvolvimento do conteudo

---
+++ resources/views/home/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.principal')
+@section('content')
+<div class="content">
+
+    <h1>Danilo Scipioni</h1>
+    <section>
+        <p> Testando Paragrafo </p>
+        kasdjfasjhdlajhdsfa ashdfkasdjfasjhdlajhdsfa ashdfkasdjfasjhdlajhdsfa ashdfkasdjfasjhdlajhdsfa ashdfkasdjfasjhdlajhdsfa ashdfkasdjfasjhdlajhdsfa ashdfkasdjfasjhdlajhdsfa ashdfkasdjfasjhdlajhdsfa ashdfkasdjfasjhdlajhdsfa ashdfkasdjfasjhdlajhdsfa ashdf
+    </section>
+</div>
+@stop
--- resources/views/layout/principal.blade.php
+++ resources/views/layout/principal.blade.php
@@ -1,23 +1,33 @@
 <!doctype html>
 <html lang="en">
 
-
 <html>
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
     <link rel="stylesheet" href="css/portifolio.css">
     <title>Danilo Scipioni - Portifólio</title>
 </head>
 
 <body>
-    <div id="sidebar">
-        @yield('sidebar')
-    </div>
-    <div id="content" class="content">
-        teste 
-    </div>
+  
+    <div class="nav-toggle expanded">
+    <div class="nav-toggle-bar"></div>
+</div>
+
+<nav class="nav expanded">
+    <div class="circle"></div>
+    <ul>
+        <li><a href="#" >Sobre Mim</a></li>
+        <li><a href="#">Projetos</a></li>
+        <li><a href="#">Contato</a></li>
+    </ul>
+</nav>
+   
+@yield('content')
+
 </body>
 <script src="js/nav.js"></script>
 
