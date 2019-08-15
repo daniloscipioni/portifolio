3abf5964a5283b656ac6caf83a5984fea46de6da
Danilo Eduardo <daniloedu@gmail.com>
2019-08-14 22:54:40 -0300

    ajuste de importacao de libs

--- public/js/portifolio.js
+++ public/js/portifolio.js
@@ -1,36 +1,33 @@
- function insertPost() {
+//  function insertPost() {
 
-     var testData = {
-         firstName: 'Roberto da Silva',
-         lastName: 'Cunha'
-     }
-
-
-     $.ajax({
-         headers: {
-             'Content-Type': 'application/json'
-         },
-         url: 'http://localhost:8080/WebRestSchott/rest/helloWorld/teste',
-         type: 'POST',
-         data: JSON.stringify(testData),
-
-         beforeSend: function() {
-             console.log('Carregando...');
-         },
-         success: function(retorno) {
-             alert(retorno);
-         },
-         error: function(a, b, c) {
-             alert('Erro: ' + a['status'] + ' ' + c);
-         }
-     });
-
- }
-
-  $( document ).ready(function() {
+//      var testData = {
+//          firstName: 'Roberto da Silva',
+//          lastName: 'Cunha'
+//      }
 
 
+//      $.ajax({
+//          headers: {
+//              'Content-Type': 'application/json'
+//          },
+//          url: 'http://localhost:8080/WebRestSchott/rest/helloWorld/teste',
+//          type: 'POST',
+//          data: JSON.stringify(testData),
 
+//          beforeSend: function() {
+//              console.log('Carregando...');
+//          },
+//          success: function(retorno) {
+//              alert(retorno);
+//          },
+//          error: function(a, b, c) {
+//              alert('Erro: ' + a['status'] + ' ' + c);
+//          }
+//      });
+
+//  }
+
+ var generateGraph = function teste() {
     var mainScreen = $( '#main' ),
         height = $( '#main' ).height(),
         width = $( '#main' ).width()
@@ -42,9 +39,6 @@
             height:'150px',
             class: 'bars'
         }).appendTo(mainScreen);
-
-   alert(width + " x " + height);
-
-  // mainScreen.css({"background-color": "#ffe"});
   
-});
+        $('#bar').fadeIn(1500);
+ };
--- resources/views/more/main.blade.php
+++ resources/views/more/main.blade.php
@@ -1,14 +1,17 @@
 @extends('layout.principal')
 @section('content')
+
 <div class="nav-content expanded">
-   
+
     <div id="main">
 
-    <div id="footer">
-        
-    </div>
+        <div id="footer">
+        </div>
     </div>
 
-
+    <script>
+        generateGraph();
+    </script>
 </div>
+
 @stop
